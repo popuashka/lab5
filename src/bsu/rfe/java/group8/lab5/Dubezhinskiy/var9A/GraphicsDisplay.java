61a5264c51b00921cc02d61117619a16ece44126
popuashka <dubezhinskiy96@mail.ru>
2021-05-27 17:16:13 +0300

    GraphicsDisplay

--- src/bsu/rfe/java/group8/lab5/Dubezhinskiy/var9A/GraphicsDisplay.java
+++ src/bsu/rfe/java/group8/lab5/Dubezhinskiy/var9A/GraphicsDisplay.java
@@ -32,3 +32,16 @@ public class GraphicsDisplay extends JPanel {
     private boolean changeMode = false;
     private double[] originalPoint = new double[2];
     private Rectangle2D.Double selectionRect = new Rectangle2D.Double();
+
+    public GraphicsDisplay() {
+        this.setBackground(Color.white);
+        graphicsStroke = new BasicStroke(5.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND, 10.0f, new float[] {30, 10, 20, 10, 10, 10, 20, 10}, 0.0f);
+        this.axisStroke = new BasicStroke(2.0F, 0, 0, 10.0F, (float[])null, 0.0F);
+        this.markerStroke = new BasicStroke(4.0F, 0, 0, 10.0F, (float[])null, 0.0F);
+        this.selectionStroke = new BasicStroke(1.0F, 0, 0, 10.0F, new float[]{10.0F, 10.0F}, 0.0F);
+        this.axisFont = new Font("Serif", 1, 36);
+        this.labelsFont = new Font("Serif", 0, 20);
+        formatter.setMaximumFractionDigits(5);
+        this.addMouseListener(new GraphicsDisplay.MouseHandler());
+        this.addMouseMotionListener(new GraphicsDisplay.MouseMotionHandler());
+    }
